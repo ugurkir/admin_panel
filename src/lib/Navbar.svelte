6700bb7b71f22081269b137dd57e8e7aa7f593db
ugur <ugurkir289@gmail.com>
2023-10-09 17:19:53 +0300

    d

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,17 +1,19 @@
 <script>
     let isOpen = true;
+    import { LightSwitch } from '@skeletonlabs/skeleton';
   </script>
   
   <div class="flex">
     
     <div class={`w-64 h-screen bg-gray-800 text-white fixed top-0 left-0 transition-transform duration-200 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
       <div class="p-6">
-        <h2 class="text-2xl font-semibold mb-2">aa</h2>
+        <h2 class="text-2xl font-semibold mb-5">aa</h2>
+       
         <ul>
-          <li class="mb-1"> <a href="/Users">Users</a></li>
-          <li class="mb-1"> <a href="/Game_tables">Game tables</a></li>
+          <li class="mb-2"> <a href="/Users">Users</a></li>
+          <li class="mb-2"> <a href="/Game_tables">Game tables</a></li>
         </ul>
-      </div>
+      </div> <LightSwitch />
     </div>
   
     
